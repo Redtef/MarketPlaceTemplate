58f2d3bf062a8eb749efae93f33e27886c60a5da
Boss <Boss@DESKTOP-LVLKH7S>
2018-02-28 00:21:59 +0000

    html to php centralised navbar deleted unused html files

---
+++ ACmaintenance.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>BricolageApp - Template</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/modern-business.css" rel="stylesheet">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+    </head>
+
+    <body style="background-color: #ebebeb; ">
+
+        <!-- Navigation -->
+        <?php
+        include_once './navbar.html';
+        ?>
+        <header style=" padding-top: 5rem;padding-bottom: 2rem;">
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-xl-12 text-center">
+                        <!--style="-webkit-text-stroke:1px black; font-family: "Times New Roman", Times, serif;"-->
+                        <h1 class="mb-1">Reservez la meilleur maintenance  climatiseur</h1>
+                        <h4></h4>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container-fluid">
+            <div class="row">
+                <!-- <div class="col-md-3 ">
+                    <div class="card card-inverse">
+                        <a href="#"><img class="card-img-top" src="images/tools.jpg" height="" width="" alt=""></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title">
+                                <a href="#">Travaux divers</a>
+                            </h4>
+
+                        </div>
+                    </div>
+                </div> -->
+
+                <div class="col-md-4 h-100" style="padding-left: 20px;">
+                    <div class="card card-inverse">
+                        <img class="card-img" src="images/clim.jpg" alt="Card image">
+                        <div class="card-img-overlay">
+                            <!-- <h4 class="card-title">Card title</h4>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h4>Choisissez le pack correspondant a votre cas</h4>
+                    <div class="card-block">
+
+                        <div class="container">
+                            <div class="row" style="background-color: #edebeb; padding-top: 10px;">
+                                <div class="col-md-8">
+                                    <h6><i class="fas fa-building"></i> Maintenance climatiseur appartement</h6>
+                                </div>
+                                <div class="col-md-1">
+
+                                </div>
+                                <div class="col-md-3 pull-left">
+                                    <a href="#" ><h6>Contenu du pack?</h6></a>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  1 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="cleaningpool2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  2 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="cleaningpool2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  3 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="cleaningpool2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  4 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="acmaintenance2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="card-block">
+
+                        <div class="container">
+                            <div class="row" style="background-color: #edebeb; padding-top: 10px;">
+                                <div class="col-md-8">
+                                    <h6><i class="fas fa-home"></i> Maintenance climatiseur Villa</h6>
+                                </div>
+                                <div class="col-md-1">
+
+                                </div>
+                                <div class="col-md-3 pull-left">
+                                    <a href="#" ><h6>Contenu du pack?</h6></a>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  1 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="acmaintenance2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  2 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="acmaintenance2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  3 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="acmaintenance2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="card text-center" style="margin-bottom: 25px; margin-top: 15px; padding: 15px 10px 10px ">
+                                        <div class="card-block">
+                                            <h6 class="card-title">  4 unit</h6>
+                                            <p class="card-text">  300dh</p>
+                                            <a href="acmaintenance2.php" class="btn btn-primary btn-block"> <strong>Select</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-block" style="margin-top: 10%; margin-bottom: 10%">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i> Filter Cleaning
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Vent cleaning
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Water tray cleaning
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Drain pipe flushing
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Gas refill 
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Drain pipe flushing
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <i class="fas fa-check-circle"></i>Maintenance checks of all electrical systems
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="card-block" style="padding: 20px 20px 20px 20px">
+                    <div class="card-block">
+                        <h4 class="card-title"> FAQ</h4>
+                        <p class="card-text">hna les questions FAQ</p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- Footer -->
+        <footer class="py-5 bg-dark">
+
+            <div class="container">
+                <div class="m-0 text-center text-white" >
+                    <a class="navbar-brand" href="index.html">
+                        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                            <div class="flipper">
+                                <div class="front">
+                                    <img style="float: center;position: center;" src="images/logo.png" class="front" height="50px" width="50px">
+                                </div>
+                                <div class="back">
+                                    <!-- back content -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- /.container -->
+        </footer>
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+</html>
---
+++ Handyman.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ServiceMarket</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="css/jquery.datetimepicker.css">
+        <link href="css/modern-business.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Favicon and touch icons -->
+
+    </head>
+
+
+    <body style="background-color: #ebebeb;">
+        
+        <?php
+        include_once 'navbar.html';
+        ?>
+        
+        <header style=" padding-top: 8rem;padding-bottom: 8rem;">
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-xl-12">
+                        <!--style="-webkit-text-stroke:1px black; font-family: "Times New Roman", Times, serif;"-->
+                        <h1 class="mb-1">Obtenez les meilleurs service au Maroc</h1>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="images/icons/worker2.jpg" height="" width="" alt=""></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="#">Travaux divers</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="images/icons/electrician.jpg" alt=""></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="#">Electricien</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="images/icons/plumber.jpg" alt=""></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="plombier.php">Plombier</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="images/icons/carpenter.png" alt=""></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="#">Menuisier</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="images/icons/locksmith.png" alt=""></a>
+                        <div class="card-body"  style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="#">Serrurier</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"> <img class="card-img-top" src="images/icons/tv.png" alt=""> </a>
+                        <div class="card-body"  style="background-color: #cfcfcf;">
+                            <h4 class="card-title"><a href="#">Reparation et Montage TV</a></h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 portfolio-item">
+                    <div class="card h-100">
+                        <a href="#"><img src="images/icons/curtain.jpg" class="card-img-top"></a>
+                        <div class="card-body" style="background-color: #cfcfcf;">
+                            <h4 class="card-title"> <a href="#">Installation rideaux</a></h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+
+</html>
